c8a76bf06b9e50d621049dc380f2697959d632ee
TiborSprohar <tibor.sprohar@gmail.com>
2021-09-27 22:23:35 +0200

    Open firewall and configure http

--- LAMP.sh
+++ LAMP.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 #Installing LAMP on server
 
-sudo yum install httpd firewalld
+sudo yum install httpd firewalld vim
 sudo systemctl enable firewalld
 sudo systemctl start firewalld
+sudo firewall-cmd --zone=public --add-service=https --permanent
+sudo firewall-cmd --reload
+sudo systemctl enable httpd
+ sudo systemctl start httpd
